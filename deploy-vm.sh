1399462ee50ce40dfe5d5e9845d01f36d18d3b64
bloombytes <36093498+bloombytes@users.noreply.github.com>
2023-07-09 20:38:30 -0500

    cleanup

--- deploy-vm.sh
+++ deploy-vm.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Check for root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
 fi
 
-# Check for arguments
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <vmid> </path/to/iso>"
     exit 1
@@ -15,18 +13,14 @@ fi
 vmid=$1
 iso=$2
 
-# Create VM
 echo "Creating VM..."
 qm create $vmid --name kali --ostype l26 --ide2 local:iso/$(basename $iso),media=cdrom
 
-# Start VM
 echo "Starting VM..."
 qm start $vmid
 
-# Wait for VM to start
 sleep 10
 
-# Install and configure Tor
 echo "Installing and configuring Tor..."
 qm terminal $vmid << EOF
 apt-get update
@@ -36,18 +30,15 @@ sed -i 's/#CookieAuthentication 1/CookieAuthentication 1/' /etc/tor/torrc
 systemctl restart tor
 EOF
 
-# Install torify and proxychains
 echo "Installing torify and proxychains..."
 qm terminal $vmid << EOF
 apt-get install torify proxychains -y
 EOF
 
-# Isolate VM
 echo "Isolating VM..."
 echo "cpu: host,hidden=1" >> /etc/pve/qemu-server/$vmid.conf
 echo "args: -cpu 'host,-hypervisor'" >> /etc/pve/qemu-server/$vmid.conf
 
-# Harden VM
 echo "Hardening VM..."
 qm terminal $vmid << EOF
 apt-get install aide rkhunter chkrootkit -y
@@ -55,7 +46,6 @@ apt-get update && apt-get upgrade -y
 passwd
 EOF
 
-# Create golden image
 echo "Creating golden image..."
 vzdump $vmid --mode stop --compress lzo --storage local
 
